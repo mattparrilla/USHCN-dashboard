47e07b23ce2dd38e075e62a13e30df7d70f52af1
Joe di Stefano <joeydi@okaypl.us>
2014-11-24 18:45:11 -0500

    webargs

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, send_file
+from webargs import Arg
+from webargs.flaskparser import use_args
 from csv_to_png import make_image
 from cStringIO import StringIO
 import json
+
+from pprint import pprint
 
 app = Flask(__name__)
 
@@ -28,10 +32,28 @@ def serve_pil_image(pil_img):
     return send_file(img_io, mimetype='image/png')
 
 
+image_args = {
+    'f_n': Arg(str, default='btv'),
+    'fill_null': Arg(bool, default=True),
+    'unit': Arg(str, default='day'),
+    'smooth_horizontal': Arg(bool, default=True),
+    'smooth_vertical': Arg(bool, default=True),
+    'palette': Arg(str, default='Set1'),
+    'bins': Arg(str, default='8'),
+    'x_d': Arg(int, default=2),
+    'y_d': Arg(int, default=4),
+    'continuity': Arg(float, default=0.2),
+    'recursion': Arg(int, default=3),
+    'start_idx': Arg(bool, default=False),
+    'save_image': Arg(bool, default=False),
+}
+
 @app.route('/img/')
-def get_image():
-    image, name = make_image(**request.args.to_dict())
-    print request.args
+@use_args(image_args)
+def get_image(args):
+
+    image, name = make_image(args)
+    pprint(args)
     print name
     return serve_pil_image(image)
 
